5c637ec3ba3e5aefa620354b23af38fb878d7812
Natasha Matthews <natashamatthews90@gmail.com>
2022-12-18 12:03:51 -0600

    added in a function to index.php

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,10 @@
-This is our completely amazing custom theme.
+<?php
+    function greet($name, $color) {
+        echo "<p>Hi, My name is $name and my favorite color is $color.</p>";
+    }
+    greet('John', 'blue' );
+    greet('Jane', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
