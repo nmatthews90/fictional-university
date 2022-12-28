8994261bae9f2e4c2a8bfdacaac075ca45e07441
Natasha Matthews <natashamatthews90@gmail.com>
2022-12-28 10:56:15 -0600

    added a url rewrite to use events in the peramlinks

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -15,7 +15,7 @@ get_header();
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo site_url('/event'); ?>">
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/events'); ?>">
             <i class="fa fa-home" aria-hidden="true"></i> Event Home</a> 
             <span class="metabox__main"><?php the_title(); ?></span>
         </p>
