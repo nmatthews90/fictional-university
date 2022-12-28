eb12fcfcc775d706ce10b396c8f739a7ffe07d91
Natasha Matthews <natashamatthews90@gmail.com>
2022-12-28 11:03:08 -0600

    made the archive event page and updated the single event link

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -15,7 +15,7 @@ get_header();
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo site_url('/events'); ?>">
+          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
             <i class="fa fa-home" aria-hidden="true"></i> Event Home</a> 
             <span class="metabox__main"><?php the_title(); ?></span>
         </p>
