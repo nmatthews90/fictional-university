85afb2e6f983675dc1564623572ad641e4f4fa1b
Natasha Matthews <natashamatthews90@gmail.com>
2022-12-29 15:20:09 -0600

    linked to past events page from events archive page

--- app/public/wp-content/themes/fictional-university-theme/archive-event.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-event.php
@@ -35,6 +35,10 @@ get_header();
       echo paginate_links();
 
     ?>
+    <hr class="section-break">
+    <p>
+      Looking for a recap of past events? <a href="/past-events">Check out our past events archive.</a>
+    </p>
    </div>
 
 <?php
--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -16,7 +16,6 @@ get_header();
         $today = date('Ymd');
             $pastEvents = new WP_Query(array(
               'paged' => get_query_var('paged', 1),
-              'posts_per_page' => 1,
               'post_type' => 'event',
               'meta_key' => 'event_date',
               'orderby' => 'meta_value_num',
