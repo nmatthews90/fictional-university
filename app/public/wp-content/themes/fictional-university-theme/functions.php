12acbf4c1c8cab730fed1199ac66070f1916427d
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-07 14:31:15 -0600

    made new programs pages and edited the main query

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -20,6 +20,13 @@ function univeristy_features() {
 add_action('after_setup_theme', 'univeristy_features');
 
 function universtiy_adjust_queries($query) {
+    
+    if(!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
     if (!is_admin() AND is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
         $query->set('meta_key', 'event_date');
