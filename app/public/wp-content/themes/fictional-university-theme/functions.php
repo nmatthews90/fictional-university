d161188b37211e8495f9f429fbaa281614da436c
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-16 14:57:22 -0600

    added new images sizes to the theme and added professor pictures to the single program page: '

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -17,6 +17,8 @@ function univeristy_features() {
     
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
 }
 add_action('after_setup_theme', 'univeristy_features');
 
