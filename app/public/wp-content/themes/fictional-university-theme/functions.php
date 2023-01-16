d13056403e8e5f01cdbbfb2ea3259bcb0312b6cc
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-16 14:18:37 -0600

    added featured images to theme

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -16,6 +16,7 @@ function univeristy_features() {
     register_nav_menu('footerLocationTwo', 'Footer Location Two');
     
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'univeristy_features');
 
