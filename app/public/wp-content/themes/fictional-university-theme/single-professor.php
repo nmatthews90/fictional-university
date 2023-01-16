957ee54d9d73220e0b7c88d45033acb9a6586ab1
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-16 13:24:27 -0600

    added list of professors to the program page

--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -23,7 +23,7 @@ get_header();
       
       if ($relatedPrograms) {
         echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+        echo '<h2 class="headline headline--medium">Subjects Taught</h2>';
         echo '<ul class="link-list min-list">';
         foreach($relatedPrograms as $program) { ?>
           <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
