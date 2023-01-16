b9e6cee80e9921e68785ac56c048a9b66e49802c
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-16 15:08:21 -0600

    added new crop image plugin

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -16,7 +16,7 @@ function univeristy_features() {
     register_nav_menu('footerLocationTwo', 'Footer Location Two');
     
     add_theme_support('title-tag');
-    
+
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
