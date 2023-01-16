ff5c8582ef72a01122daf15beffa732136ac9b9c
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-16 15:01:54 -0600

    pulled the new images sizes in the program and prefessor page

--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -17,7 +17,7 @@ get_header();
         <div class="generic-content">
             <div class="row group">
                 <div class="one-third">
-                    <?php the_post_thumbnail(); ?>
+                    <?php the_post_thumbnail('professorPortrait'); ?>
                 </div>
                 <div class="two-thirds">
                     <?php the_content(); ?>
