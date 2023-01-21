5fede1fe523f018d691fb8ed6f4a57725abdbef2
Natasha Matthews <natashamatthews90@gmail.com>
2023-01-21 11:52:02 -0600

    added pageBanner function to rest of the template files

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -2,16 +2,16 @@
 
 function pageBanner($args = NULL) {
   
-  if (!$args['title']) {
+  if (!isset($args['title'])) {
     $args['title'] = get_the_title();
   }
 
-  if (!$args['subtitle']) {
+  if (!isset($args['subtitle'])) {
     $args['subtitle'] = get_field('page_banner_subtitle');
   }
 
-  if (!$args['photo']) {
-    if (get_field('page_banner_background_image')) {
+  if (!isset($args['photo'])) {
+    if (get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
       $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
     } else {
       $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
