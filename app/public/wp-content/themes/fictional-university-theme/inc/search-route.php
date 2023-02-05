2ffc7516e0ebd7418f378e1cd2738014adfeb06e
Natasha Matthews <natashamatthews90@gmail.com>
2023-02-05 11:28:33 -0600

    added campuses and events to the search overlay

--- app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -44,6 +44,17 @@ function universitySearchResults($data) {
         }
 
         if (get_post_type() == 'program') {
+            $relatedCampuses = get_field('related_campus');
+            if($relatedCampuses) {
+                foreach($relatedCampuses as $campus) {
+                    array_push($results['campuses'],array(
+                         'title' => get_the_title($campus),
+                          'permalink' => get_the_permalink($campus),
+                    ));
+                }
+            }
+            
+
             array_push($results['programs'], array(
             'title' => get_the_title(),
             'permalink' => get_the_permalink(),
@@ -89,7 +100,7 @@ function universitySearchResults($data) {
     }
     
     $programRelationshipQuery = new WP_Query(array(
-        'post_type' => 'professor',
+        'post_type' => array('professor', 'event'),
         'meta_query' => $programsMetaQuery
     ));
 
@@ -101,11 +112,31 @@ function universitySearchResults($data) {
             'permalink' => get_the_permalink(),
             'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
         ));
+
+       if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = null;
+            if (has_excerpt()) {
+                $description = get_the_excerpt();
+            } else {
+                $description = wp_trim_words(get_the_content(), 18);
+            }
+
+            array_push($results['events'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'month' => $eventDate->format('M'),
+            'day' => $eventDate->format('d'),
+            'description' => $description
+        )); 
+        }
+
         }
     }
 
     //removes duplicates
     $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
 
     }
     
