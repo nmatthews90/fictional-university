dbd9b08857e3352260ba0664643c7a28393ef34a
Natasha Matthews <natashamatthews90@gmail.com>
2023-02-07 13:21:27 -0600

    added new plugin and added new capabilities to campus and event types

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -3,6 +3,8 @@ function university_post_types() {
 
      //Campus post type
     register_post_type('campus', array(
+        'capability_type' => 'campus',
+        'map_meta_cap' => true,
         'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array(
             'slug' => 'campuses'
@@ -22,6 +24,8 @@ function university_post_types() {
 
     //Event post type
     register_post_type('event', array(
+        'capability_type' => 'event',
+        'map_meta_cap' => true,
         'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array(
             'slug' => 'events'
