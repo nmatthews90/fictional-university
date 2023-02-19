fcbf3ffc83d40f32816c36c3868997b33a9fda9a
Natasha Matthews <natashamatthews90@gmail.com>
2023-02-19 10:25:48 -0600

    sanitize the text areas

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -149,6 +149,10 @@ function ourLoginTitle() {
 //force note posts to be private
 add_filter('wp_instert_post_data', 'makeNotePrivate');
 function makeNotePrivate($data) {
+  if($data['post_type'] == 'note') {
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
   if($data['post_type'] == 'note' and $data['post_status'] != 'trash') {
     $data['post_status'] = "private";
   }
