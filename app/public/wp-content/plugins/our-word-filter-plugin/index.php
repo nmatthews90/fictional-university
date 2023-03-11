72ca9aa42fa15d3bae826201cd4b7fa625f756a2
Natasha Matthews <natashamatthews90@gmail.com>
2023-03-11 10:58:41 -0600

    added sub menu to word filter

--- app/public/wp-content/plugins/our-word-filter-plugin/index.php
+++ app/public/wp-content/plugins/our-word-filter-plugin/index.php
@@ -10,3 +10,26 @@
 
 if( ! defined( 'ABSPATH')) exit; // Exit of accessed directly
 
+class OurWordFilterPlugin {
+    function __construct()
+    {
+        add_action('admin_menu', array($this, 'ourMenu'));
+    }
+
+    function ourMenu() {
+        add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
+        add_submenu_page('ourwordfilter', 'Words To Filter', 'Words List', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'));
+        add_submenu_page('ourwordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionsSubPage'));
+    }
+
+    function wordFilterPage() { ?>
+        hello world
+    <?php }
+
+    function optionsSubPage() { ?>
+        hello world options sub page
+    <?php }
+}
+
+$ourWordFilterPlugin = new OurWordFilterPlugin();
+
