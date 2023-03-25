62b562f4a9a5dcfd74016ef1bcc767686eb268ea
Natasha Matthews <natashamatthews90@gmail.com>
2023-03-25 13:40:15 -0500

    added sky color and grass color to block

--- app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '8efee5acd9b0f80dab82');
+<?php return array('dependencies' => array('wp-element'), 'version' => '105df8fc4ddd8e77e68b');
--- app/public/wp-content/plugins/are-you-paying-attention/src/index.js
+++ app/public/wp-content/plugins/are-you-paying-attention/src/index.js
@@ -2,15 +2,40 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
   title: "Are You Paying Attention?",
   icon: "smiley",
   category: "common",
-  edit: function () {
+  attributes: {
+    skyColor: { type: "string" },
+    grassColor: { type: "string" },
+  },
+  edit: function (props) {
+    function updateSkyColor(event) {
+      props.setAttributes({ skyColor: event.target.value });
+    }
+    function updateGrassColor(event) {
+      props.setAttributes({ grassColor: event.target.value });
+    }
     return (
       <div>
-        <p>Hello, this is a paragraph. </p>
-        <h4>Hi there.</h4>
+        <input
+          type="text"
+          placeholder="sky color"
+          value={props.attributes.skyColor}
+          onChange={updateSkyColor}
+        />
+        <input
+          type="text"
+          placeholder="grass color"
+          value={props.attributes.grassColor}
+          onChange={updateGrassColor}
+        />
       </div>
     );
   },
-  save: function () {
-    return wp.element.createElement("h1", null, "this is the front end");
+  save: function (props) {
+    return (
+      <p>
+        Today the sky is {props.attributes.skyColor} and the grass is{" "}
+        {props.attributes.grassColor}.
+      </p>
+    );
   },
 });
