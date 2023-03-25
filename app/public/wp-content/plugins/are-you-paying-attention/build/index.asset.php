08b430cb012f52c32734e659efe8d55f4eff2ec9
Natasha Matthews <natashamatthews90@gmail.com>
2023-03-25 15:12:11 -0500

    used php to display block attributes on the front end

--- app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '105df8fc4ddd8e77e68b');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'fac15023bfa2c7bc0cf1');
--- app/public/wp-content/plugins/are-you-paying-attention/build/index.js
+++ app/public/wp-content/plugins/are-you-paying-attention/build/index.js
@@ -126,7 +126,7 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
     }));
   },
   save: function (props) {
-    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, "Today the sky is ", props.attributes.skyColor, " and the grass is", " ", props.attributes.grassColor, ".");
+    return null;
   }
 });
 })();
