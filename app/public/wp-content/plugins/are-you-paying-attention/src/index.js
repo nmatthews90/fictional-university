205bf7da7b1e6487662b0d703d8bd39d9793076f
Natasha Matthews <natashamatthews90@gmail.com>
2023-03-26 12:11:30 -0500

    added classes to the css

--- app/public/wp-content/plugins/are-you-paying-attention/src/index.js
+++ app/public/wp-content/plugins/are-you-paying-attention/src/index.js
@@ -31,21 +31,24 @@ function EditComponent(props) {
   }
   return (
     <div className="paying-attention-edit-block">
-      <TextControl label="Question: " />
-      <p>Answers:</p>
+      <TextControl label="Question:" style={{ fontSize: "20px" }} />
+      <p style={{ fontSize: "13px", margin: "20px 0px 0px 0px" }}>Answers:</p>
       <Flex>
         <FlexBlock>
           <TextControl />
         </FlexBlock>
         <FlexItem>
           <Button>
-            <Icon icon="star-empty" />
+            <Icon className="mark-as-correct" icon="star-empty" />
           </Button>
         </FlexItem>
         <FlexItem>
-          <Button> Delete </Button>
+          <Button variant="link" className="attention-delete">
+            Delete
+          </Button>
         </FlexItem>
       </Flex>
+      <Button isPrimary>Add another answer</Button>
     </div>
   );
 }
